6afbc00a82310bd2afa69a1efe07459238c4011a
SILLAS ROCHA DA COSTA <B51073@fgv.edu.br>
2023-11-14 12:38:39 -0300

    adicionei o comando de comentários de espectadores

--- sqls/instrucoes.sql
+++ sqls/instrucoes.sql
@@ -17,3 +17,23 @@ ORDER BY visualizacoesclipes desc;
 (4 rows)
 */
 
+
+/*
+Instrução SQL que mostra as informações dos comentários de espectadores. Mostra a quantidade de bits doados e o número total de comentários, agrupando pelo nome do usuário que fez o comentário. 
+*/
+SELECT u.nomeusuario, SUM(c.quantidadebits) AS BitsDoados, COUNT(c.idusuarioespectador) AS NumComentarios
+FROM usuario u, comentario c
+WHERE u.idusuario = c.idusuarioespectador
+GROUP BY u.nomeusuario
+ORDER BY BitsDoados desc;
+
+/* Resultado: 
+    nomeusuario    | bitsdoados | numcomentarios
+-------------------+------------+----------------
+ XipXapXD          |       1000 |              1
+ Sillas_fps_2011   |        150 |              2
+ Pedro_games       |        150 |              2
+ marçadovolei      |          0 |              2
+ Luizinha_gameplay |          0 |              1
+(5 rows)
+*/
