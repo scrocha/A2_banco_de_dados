5928713db457829ec63e2eeec89b29c46a53e1c2
Sillas Rocha <125564261+scrocha@users.noreply.github.com>
2023-11-21 11:29:30 -0300

    Update index.sql

--- sqls/index.sql
+++ sqls/index.sql
@@ -1,2 +1,5 @@
+/*
+Cria um Índice na coluna NomeUsuario da tabela Usuario, uma coluna que pode ser usada para quase todas as pesquisas, já que tanto canais quanto espectadores possuem nome de usuário.
+*/
 
-CREATE INDEX Nome_de_Usuario ON Twitch.Usuario (NomeUsuario);
+CREATE INDEX Nome_de_Usuario ON Twitch.Usuario (NomeUsuario DESC);
